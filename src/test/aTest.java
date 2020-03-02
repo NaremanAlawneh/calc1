a63dca111de652b6f6d0b89987707eb4fc77e17c
nreman <AMsys@DESKTOP-O34V0M5.home>
2020-03-02 20:46:30 +0200

    commit

--- src/test/aTest.java
+++ src/test/aTest.java
@@ -2,13 +2,36 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.calc;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class aTest {
 
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testSum_BothNumbersArePositive_ShouldReturnPositiveNumber() {
+		// Arrange
+		int a = 10;
+		int b = 20;
+		calc c = new calc();
+	
+		int result = c.add(a, b);
+	
+		Assert.assertTrue(result > 0);
 	}
 
+	@Test
+	public void testSum_BothNumbersAreNegative_ShouldReturnNegNumber() {
+		// Arrange
+		int a = -10;
+		int b = -20;
+		calc c = new calc();
+	
+		int result = c.add(a, b);
+	
+		Assert.assertTrue(result < 0);
+	}
+	
 }
